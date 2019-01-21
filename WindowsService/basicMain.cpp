8bf6c918f0ebcc18cb1b36b617b437bbe01cc31b
bys <410389821@qq.com>
2019-01-21 11:14:23 +0800

    使用非c++11方式创建线程

--- WindowsService/basicMain.cpp
+++ WindowsService/basicMain.cpp
@@ -1,11 +1,37 @@
-#include "basicMain.h"
-#include <windows.h>
-#include <thread>
-#include <fstream>
 #include <iostream>
+#include <process.h>
+#include "basicMain.h"
+
+bool g_exitFlag = true;//退出标记，false为退出
+
+unsigned int _stdcall service_thread_func(void *exitFlag)
+{
+	//线程函数，执行程序主任务
+	while (*(bool*)exitFlag)
+	{
+		//执行任务
+		std::ofstream ofs("\\custom_service.txt", std::ios::app);
+		if (!ofs)
+		{
+			return;
+		}
+		int i = 0;
+		while (i < 100)
+		{
+			ofs << "第 " << ++i << " 次" << std::endl;
+			Sleep(1000);
+		}
+		ofs.close();
+
+		Sleep(1000);
+	}
+
+	return 0;
+}
 
 BasicMain::BasicMain(const char* serviceName, const char* displayName)
 	:ServiceBase(serviceName, displayName)
+	,m_threadHandle(NULL)
 {
 }
 
@@ -13,24 +39,15 @@ BasicMain::~BasicMain()
 {
 }
 
-void BasicMain::service_thread_func()
-{
-	std::ofstream ofs("\\custom_service.txt", std::ios::app);
-	if (!ofs)
-	{
-		return;
-	}
-	int i = 0;
-	while (i < 100)
-	{
-		ofs << "第 " << ++i << " 次" << std::endl;
-		Sleep(1000);
-	}
-	ofs.close();
-}
-
 void BasicMain::OnStart(DWORD argc, TCHAR * argv[])
 {
-	std::thread service_thread(service_thread_func);
-	service_thread.detach();
+	// Create thread.
+	m_threadHandle = (HANDLE)_beginthreadex(NULL, 0, service_thread_func, (void *)&g_exitFlag, 0, NULL);
+}
+
+void BasicMain::OnStop()
+{
+	g_exitFlag = false;//退出标记
+	WaitForSingleObject(m_threadHandle, INFINITE);//等待线程结束
+	CloseHandle(m_threadHandle);//关闭线程句柄
 }
--- WindowsService/basicMain.h
+++ WindowsService/basicMain.h
@@ -3,15 +3,19 @@
 
 #include "serviceBase.h"
 
-void custom_service();
+extern bool g_exitFlag;//线程退出标记，false为退出
+unsigned int _stdcall service_thread_func(void *exitFlag);//线程入口函数
 
 class BasicMain : public ServiceBase{
 public:
 	BasicMain(const char* serviceName, const char* displayName);
 	~BasicMain();
 
-	static void service_thread_func();
-	void OnStart(DWORD argc, TCHAR* argv[]);
+protected:
+	void OnStart(DWORD argc, TCHAR* argv[]);//重写基类虚函数，在该函数执行主要任务
+	void OnStop();//重写基类虚函数，在该函数中执行服务停止需要处理的事项
+private:
+	HANDLE m_threadHandle;//线程句柄
 };
 
 #endif	//BASICMAIN_H
